14b5fd7cf5bbc54c8a71112272a910aeab95047b
Humberto Rocha <humrochagf@gmail.com>
2017-10-24 12:53:28 -0200

    Consistency adjustment

--- tapioca_discourse/__init__.py
+++ tapioca_discourse/__init__.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
 __author__ = 'Humberto Rocha'
 __email__ = 'humrochagf@gmail.com'
--- tapioca_discourse/tapioca_discourse.py
+++ tapioca_discourse/tapioca_discourse.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
 from tapioca import (JSONAdapterMixin, TapiocaAdapter,
                      generate_wrapper_from_adapter)
