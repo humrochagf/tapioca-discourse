246a45834abd75a8afd2c5039bdee272d69644b7
Gabriel Carvalho <gblmiranda@MacBook-Pro-de-Gabriel.local>
2017-10-24 21:36:41 -0200

    add admin emails resources

---
+++ tapioca_discourse/resource_mapping/admin_emails.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+ADMIN_EMAILS_MAPPING = {
+    'admin_emails_settings': {
+        'resource': 'admin/email.json',
+        'docs': ('http://docs.discourse.org/#tag/'
+                 'Admin-Emails%2Fpaths%2F~1admin~1email.json%2Fget'),
+        'methods': ['GET'],
+    },
+    'admin_emails_get_list_templates': {
+        'resource': 'admin/customize/email_templates.json',
+        'docs': ('http://docs.discourse.org/#tag/'
+                'Admin-Emails%2Fpaths%2F~1admin~1customize~',
+                '1email_templates.json%2Fget'),
+        'methods': ['GET'],
+    },
+}
+
