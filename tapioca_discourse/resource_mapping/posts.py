ff8635a2127cf45a2066d28bba2520990b5d16c8
GusAv <gustavo.vilhena@outlook.com>
2017-10-24 20:03:12 -0200

    posts file

---
+++ tapioca_discourse/resource_mapping/posts.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+POSTS_MAPPING = {
+    'posts_create': {
+        'resource': 'posts',
+        'docs': ('http://docs.discourse.org/#tag/'
+                 'Posts%2Fpaths%2F~1posts%2Fpost'),
+        'methods': ['POST'],
+    },
+    'posts_get': {
+        'resource': 'posts/{id}',
+        'docs': ('http://docs.discourse.org/#tag/'
+                'Posts%2Fpaths%2F~1posts~1%7Bid%7D%2Fget'),
+        'methods': ['GET'],
+    },
+    'posts_update': {
+        'resource': 'posts/{id}',
+        'docs': ('http://docs.discourse.org/#tag/'
+                 'Posts%2Fpaths%2F~1posts~1%7Bid%7D%2Fput'),
+        'methods': ['PUT'],
+    },
+    'posts_like': {
+        'resource': 'post_actions',
+        'docs': ('http://docs.discourse.org/#tag/'
+                 'Posts%2Fpaths%2F~1post_actions%2Fpost'),
+        'methods': ['POST'],
+    },
+    'posts_unlike': {
+        'resource': 'post_actions/{id}',
+        'docs': ('http://docs.discourse.org/#tag/'
+                 'Posts%2Fpaths%2F~1post_actions~1%7Bid%7D%2Fdelete'),
+        'methods': ['DELETE'],
+    },
+}
