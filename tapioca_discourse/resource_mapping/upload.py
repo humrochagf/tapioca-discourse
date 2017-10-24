2e59909ba2d513fb927937dd5a217114db3e99bf
Gabriel Carvalho <gblmiranda@MacBook-Pro-de-Gabriel.local>
2017-10-24 21:22:34 -0200

    add upload resource

---
+++ tapioca_discourse/resource_mapping/upload.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+UPLOAD_MAPPING = {
+    'upload_file': {
+        'resource': 'uploads.json',
+        'docs': ('http://docs.discourse.org/#tag/'
+                 'Upload%2Fpaths%2F~1uploads.json%2Fpost'),
+        'methods': ['POST'],
+    },
+}
+
