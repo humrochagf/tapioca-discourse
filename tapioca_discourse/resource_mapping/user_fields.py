70b8b396ae97be1d9f31ca4482caec6b0dd1a1e2
Carlos Damázio <kaduardobsb95@gmail.com>
2017-10-24 22:25:04 -0200

    User fields endpoint create and fixed typo in init.

---
+++ tapioca_discourse/resource_mapping/user_fields.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+USER_FIELDS_MAPPING = {
+    'user_fields_list': {
+        'resource': 'admin/customize/user_fields.json',
+        'docs': ('http://docs.discourse.org/#tag/'
+                 'User-Fields'),
+        'methods': ['GET'],
+    },
+    'user_fields_create': {
+        'resource': 'admin/customize/user_fields.json',
+        'docs': ('http://docs.discourse.org/#tag/'
+                'User-Fields%2Fpaths%2F~1admin~1customize~1user_fields.json%2Fpost'),
+        'methods': ['POST'],
+    },
+    'user_fields_delete': {
+        'resource': 'admin/customize/user_fields/{id}',
+        'docs': ('http://docs.discourse.org/#tag/'
+                'User-Fields%2Fpaths%2F~1admin~1customize~1user_fields~1{id}%2Fdelete'),
+        'methods': ['DELETE'],
+    },
+}
