47a736cd2753bad2a0f6c8d6b6e3d60e8b87df2d
GusAv <gustavo.vilhena@outlook.com>
2017-10-25 21:37:16 -0200

    about logs poll reports webhooks files

---
+++ tapioca_discourse/resource_mapping/about.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+ABOUT_MAPPING = {
+    'about_get': {
+        'resource': 'about.json',
+        'docs': ('http://docs.discourse.org/#tag/'
+                 'About%2Fpaths%2F~1about.json%2Fget'),
+        'methods': ['GET'],
+    },
+}
---
+++ tapioca_discourse/resource_mapping/logs.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+LOGS_MAPPING = {
+    'logs_get': {
+        'resource': 'admin/logs/staff_action_logs.json',
+        'docs': ('http://docs.discourse.org/#tag/'
+                 'Logs%2Fpaths%2F~1admin~1logs~1staff_action_logs.json%2Fget'),
+        'methods': ['GET'],
+    },
+}
