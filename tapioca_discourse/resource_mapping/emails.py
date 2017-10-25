6b43e7661510b11e19e6618ea5de1141aa6d9166
GusAv <gustavo.vilhena@outlook.com>
2017-10-25 20:51:45 -0200

    emails file

---
+++ tapioca_discourse/resource_mapping/emails.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+EMAILS_MAPPING = {
+    'emails_get': {
+        'resource': 'admin/emails/{action}.json',
+        'docs': ('http://docs.discourse.org/#tag/'
+                 'Emails%2Fpaths%2F~1admin~1emails~1%7Baction%7D.json%2Fget'),
+        'methods': ['GET'],
+    },
+}
